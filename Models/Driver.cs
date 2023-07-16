9218d89c71cb95ef07b259f6bf782a7100c53424
prudvi-m <prudhvi.manukonda4@google.com>
2023-07-16 13:46:07 -0500

    Driver model add

---
+++ Models/Driver.cs
@@ -0,0 +1,85 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.Text.RegularExpressions;
+
+namespace Drivers.Models
+{
+    public class Driver
+    {
+        public int DriverId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First Name must contain characters only.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a last name.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last Name must contain characters only.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a date of birth.")]
+        [MinimumAge(18, ErrorMessage = "Age must be 18 years or older.")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Please enter an address.")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Address must be alphanumeric.")]
+        [StringLength(50, ErrorMessage = "Address cannot exceed 50 characters.")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a social security number.")]
+        [RegularExpression(@"^\d{3}-\d{2}-\d{4}$", ErrorMessage = "Social security number must be in the format xxx-xx-xxxx.")]
+        public string SSN { get; set; }
+
+        [RegularExpression(@"^\d{5}-\d{4}$", ErrorMessage = "Post Code must be in the format xxxxx-xxxx.")]
+        public string PostCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [Remote(action: "VerifyEmail", controller: "Driver", ErrorMessage = "Email already exists.")]
+        public string Email { get; set; } = string.Empty;
+
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}|\d{3}\.\d{3}\.\d{4}$", ErrorMessage = "Phone must be in the format xxx-xxx-xxxx or xxx.xxx.xxxx.")]
+        public string Phone { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9\s-]+$", ErrorMessage = "Driving license number must be alphanumeric.")]
+        public string DriverLicense { get; set; } = string.Empty;
+
+        public string Slug =>
+            FirstName?.Replace(' ', '-').ToLower() + '-' + SSN?.ToString();
+    }
+
+    public class MinimumAgeAttribute : ValidationAttribute
+    {
+        private readonly int _minimumAge;
+
+        public MinimumAgeAttribute(int minimumAge)
+        {
+            _minimumAge = minimumAge;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateOfBirth)
+            {
+                var age = CalculateAge(dateOfBirth);
+                if (age >= _minimumAge)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return new ValidationResult(ErrorMessage);
+        }
+
+        private int CalculateAge(DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+            if (dateOfBirth.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
--- Models/UberContext.cs
+++ Models/UberContext.cs
@@ -21,11 +21,12 @@ namespace UberEats.Models
                 new Category { CategoryID = 1, Name = "Restaurant" },
                 new Category { CategoryID = 2, Name = "Grocery" },
                 new Category { CategoryID = 3, Name = "Alcohol" },
-                new Category {CategoryID=4, Name="Convienience"},
-                new Category {CategoryID=5, Name="Flower shop"},
-                new Category {CategoryID=6,Name="Pet Store"},
-                new Category {CategoryID=7,Name="retail"}
+                new Category { CategoryID= 4, Name = "Convienience"},
+                new Category { CategoryID= 5, Name = "Flower shop"},
+                new Category { CategoryID= 6,Name = "Pet Store"},
+                new Category { CategoryID= 7,Name = "retail"}
             );
+
 
             modelBuilder.Entity<Partner>().HasData(
                 new Partner
