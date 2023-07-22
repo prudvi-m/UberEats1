a0dba06347577c6d3e1e2883511344c4a5290c19
prudvi-m <prudhvi.manukonda4@google.com>
2023-07-22 02:06:52 -0500

    added login models

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UberEats.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        [RegularExpression("^[A-Za-z0-9]+$", ErrorMessage ="Field takes only alphabets and numbers")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name.")]
+        [RegularExpression("^[A-Za-z]+$", ErrorMessage ="Field takes only alphabets")]
+        [StringLength(255)]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name.")]
+        [StringLength(255)]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace UberEats.Models
+{
+    public class User : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
+
+    }
+}
